5d2fb6935891e3d4ad209d5ba870ac3dac7c94f8
Martin Broly <martinbroly@gmail.com>
2019-07-31 19:21:19 +0200

    Using SQLAlchemy to create the db

--- web_table_rendering.py
+++ web_table_rendering.py
@@ -18,7 +18,14 @@ def my_form_post():
     requested_adapted_pheno = "%" + "%".join(request.form['pheno'].split()) + "%"
     requested_adapted_source = "%" + "%".join(request.form['source'].split()) + "%"
     print(request.form)
-    cursor.execute("SELECT DISTINCT CHROM, POS, REF, ALT, QUAL, AC, AF, DP, SYMBOL, Patient_T550.Gene, SIFT, PolyPhen, MAX_AF, MAX_AF_POPS, T550GT, gene_pheno.gene, gene_pheno.pheno, source FROM Patient_T550 INNER JOIN gene_pheno ON Patient_T550.Gene = gene_pheno.gene WHERE gene_pheno.source LIKE '" + requested_adapted_source + "' AND gene_pheno.pheno LIKE '" + requested_adapted_pheno + "' AND Patient_T550.AF>" + request.form['all_fraction'] + " AND Patient_T550.MAX_AF<" + request.form['freq_cut_off'] + " ORDER BY MAX_AF")
+    cursor.execute("SELECT DISTINCT CHROM, POS, REF, ALT, QUAL, AC, AF, DP, SYMBOL, Patient_T550.Gene, SIFT, PolyPhen, MAX_AF, MAX_AF_POPS, T550GT, gene_pheno.gene, gene_pheno.pheno, source "
+                   "FROM Patient_T550 "
+                   "INNER JOIN gene_pheno ON Patient_T550.Gene = gene_pheno.gene "
+                   "WHERE gene_pheno.source LIKE '" + requested_adapted_source + "' "
+                        "AND gene_pheno.pheno LIKE '" + requested_adapted_pheno + "' "
+                        "AND Patient_T550.AF>" + request.form['all_fraction'] + " "
+                        "AND Patient_T550.MAX_AF<" + request.form['freq_cut_off'] + " "
+                    "ORDER BY MAX_AF")
     results = cursor.fetchall()
     cursor.close()
     connexion.close()
